32800a2f73582a3e3ed4ca09c49704baa0951d12
gabafo1 <gabrielafolabi@gmail.com>
2023-10-16 04:40:53 -0700

    commit

--- handle_print.c
+++ handle_print.c
@@ -1,21 +1,20 @@
 #include "main.h"
-#include <string.h>
-#include <unistd.h>
 /**
  * handle_print - Prints an argument based on its type
  * @fmt: Formatted string in which to print the arguments.
  * @list: List of arguments to be printed.
  * @ind: ind.
  * @buffer: Buffer array to handle print.
- * @flags: Calc active flags
+ * @flags: Calculates active flags
  * @width: get width.
  * @precision: Precision specification
  * @size: Size specifier
- * Return: 1 or 2
+ * Return: 1 or 2;
  */
-int handle_print(const char *fmt, int *ind, va_list list, char buffer[], int flags, int width, int precision, int size)
+int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
+	int flags, int width, int precision, int size)
 {
-	int i, unkn_len = 0, print_chars = -1;
+	int i, unknow_len = 0, printed_chars = -1;
 	fmt_t fmt_types[] = {
 		{'c', print_char}, {'s', print_string}, {'%', print_percent},
 		{'i', print_int}, {'d', print_int}, {'b', print_binary},
@@ -31,9 +30,9 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[], int fla
 	{
 		if (fmt[*ind] == '\0')
 			return (-1);
-		unkn_len += write(1, "%%", 1);
+		unknow_len += write(1, "%%", 1);
 		if (fmt[*ind - 1] == ' ')
-			unkn_len += write(1, " ", 1);
+			unknow_len += write(1, " ", 1);
 		else if (width)
 		{
 			--(*ind);
@@ -43,8 +42,8 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[], int fla
 				--(*ind);
 			return (1);
 		}
-		unkn_len += write(1, &fmt[*ind], 1);
-		return (unkn_len);
+		unknow_len += write(1, &fmt[*ind], 1);
+		return (unknow_len);
 	}
-	return (print_chars);
+	return (printed_chars);
 }
